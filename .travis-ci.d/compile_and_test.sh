e2955a09db0e1dae59f6188284bd125153f51465
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-03-01 17:48:31 +0100

    Add Werror flag to CI build, compilation keep going on error and join statements with 'and'

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -GNinja -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" ..
-ninja
-ctest --output-on-failure
+cmake -GNinja -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always -Werror" .. && \
+    ninja -k 0 && \
+    ctest --output-on-failure
 
